a8e93c45c801602e2ec34bd802c3ab732e8de281
Amadeo Haq <deo531.id@gmail.com>
2025-09-04 23:38:05 +0700

    Add new

---
+++ Camera.java
@@ -0,0 +1,12 @@
+public class Camera {
+    public void play(){
+        System.out.println("Playing");
+    }
+    public void Stop(){
+        System.out.println("Stopping");
+    }
+    public void menu(){
+        System.out.println("Menu");
+    }
+    
+}
---
+++ TV.java
@@ -0,0 +1,8 @@
+public class TV {
+    public void TV(){
+        System.out.println("TV mode");
+    }
+    public void menu(){
+        System.out.println("TV Menu");
+    }
+}
